da8ce22220156c1935ed8e278cfce8b73442dc97
TimIsOverpowered <admin@angelthump.com>
2020-12-03 12:32:07 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Frontpage from "./frontpage";
+import Vods from "./vods";
+import VodPlayer from "./vod_player";
+import Navbar from "./navbar";
+import background from "./assets/background.jpg";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <div
+              className="root background"
+              style={{
+                backgroundImage: `url(${background})`,
+              }}
+            >
+              <Navbar {...props} />
+              <Frontpage {...props} />
+            </div>
+          )}
+        />
+        <Route
+          exact
+          path="/vods"
+          render={(props) => (
+            <div
+              className="root background"
+              style={{
+                backgroundImage: `url(${background})`,
+              }}
+            >
+              <Navbar {...props} />
+              <Vods {...props} />
+            </div>
+          )}
+        />
+        <Route
+          exact
+          path="/vods/:vodId"
+          render={(props) => (
+            <div className="root">
+              <VodPlayer {...props} />
+            </div>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+}
